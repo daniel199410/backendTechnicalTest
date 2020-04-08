6b7a1c0844b7d76a29c89667e937ff9808fbf91e
Daniel Cataño <dcatanor@unal.edu.co>
2020-04-08 15:04:26 -0500

    Se crea servicio de Configuración

---
+++ configurationService/src/test/java/com/dcatano/configurationService/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcatano.configurationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
