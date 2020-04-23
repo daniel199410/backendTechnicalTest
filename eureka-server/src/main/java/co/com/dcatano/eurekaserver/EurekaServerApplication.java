a995cfe3f87128a4f630dc1468d8255836621bc3
Daniel Cataño <dcatanor@unal.edu.co>
2020-04-23 14:29:47 -0500

    Se crea el servicio de Eureka

---
+++ eureka-server/src/main/java/co/com/dcatano/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package co.com.dcatano.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
