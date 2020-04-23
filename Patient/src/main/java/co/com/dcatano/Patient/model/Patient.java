ed97964396d7724c5e9e623e1be0922bdf63ed60
Daniel Cataño <dcatanor@unal.edu.co>
2020-04-23 17:28:52 -0500

    Model, Repository for patient table

---
+++ Patient/src/main/java/co/com/dcatano/Patient/model/Patient.java
@@ -0,0 +1,80 @@
+package co.com.dcatano.Patient.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PATIENT")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "PATIENT_ID")
+    private Long patientId;
+
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ Patient/src/main/java/co/com/dcatano/Patient/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package co.com.dcatano.Patient.repository;
+
+import co.com.dcatano.Patient.model.Patient;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends CrudRepository<Patient, Long> {
+}
